d4ed0c1a38ed2bc390da4a81072f56a6059def9f
Chang <cnxiong03@gmail.com>
2020-10-08 00:15:58 -0400

    YahtzeeConsole class created

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -5,6 +5,13 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        YahtzeeConsole.welcome();
+        Hand myHand = new Hand();
+        myHand.roll(rand);
+        YahtzeeConsole.handRolled(myHand.getDice());
+        myHand.roll(rand, YahtzeeConsole.chooseDice());
+        YahtzeeConsole.handRolled(myHand.getDice());
+        myHand.roll(rand, YahtzeeConsole.chooseDice());
     }
 }
--- src/com/codewithchang/YahtzeeConsole.java
+++ src/com/codewithchang/YahtzeeConsole.java
@@ -1,9 +1,33 @@
 package com.codewithchang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class YahtzeeConsole extends Console{
     static public void welcome() {
         welcome("Yahtzee");
     }
 
+    static public void handRolled(List<Die> dice) {
+        for (Die die : dice) {
+            System.out.print(die.getValue() + " ");
+        }
+    }
 
+    static public int chooseDie() {
+        System.out.println("Choose die to reroll 1-5: ");
+        String input = scanner.next();
+        return Integer.parseInt(input) -1;
+    }
+
+    static public List<Integer> chooseDice() {
+        System.out.println("Choose the dice you want to reroll 1-5 Ex 1 2 3 4 5: ");
+        String input = scanner.nextLine();
+        String[] inputArray = input.split(" ");
+        List<Integer> choices = new ArrayList<Integer>();
+        for (String choice : inputArray) {
+            choices.add(Integer.parseInt(choice)-1);
+        }
+        return choices;
+    }
 }
